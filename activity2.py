f6289575d62cfb682203fb5cc8ffc6d9a6a07ed9
yesh20hiya-afk <yesh20hiya@gmail.com>
2026-01-21 20:34:48 -0500

    v2

---
+++ activity2.py
@@ -0,0 +1,24 @@
+a = 10
+b = 12
+c = 12
+
+print(a!=b)
+print(b!=c)
+
+a = "python"
+b = "codingal"
+
+if a != b:
+    print(a, "and", b, "are different")
+    
+    
+a = 4
+b = 5
+
+if a == 1 and b == 5:
+    print("hello")
+
+a = int(input("Enter a number: "))
+
+if a%2 != 0:
+    print(a, "is an odd number")
