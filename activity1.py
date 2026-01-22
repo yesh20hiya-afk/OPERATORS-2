d14e640af83515b62a5d7c918dc6e9e28695cd02
yesh20hiya-afk <yesh20hiya@gmail.com>
2026-01-21 20:20:56 -0500

    v1

---
+++ activity1.py
@@ -0,0 +1,29 @@
+a = 10
+b = 12
+c = 0
+
+if a and b and c:
+    print("All the numbers have boolean value True")
+    
+else:
+    print("At least one of the numbers has boolean value as False")
+    
+    
+    
+a = 10
+b = -10
+c = 0
+
+if a > 0 or b > 0 :
+
+    print("Either of the number are greater than 0")
+     
+else:
+    print("Neither of the numbers are greater than 0")
+    
+if b > 0 or c > 0 :
+
+    print("Either of the number are greater than 0")
+    
+else:
+    print("Neither of the numbers are greater than 0")
